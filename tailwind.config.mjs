9bb4be5f07f045b7a7db7b48eb33c91e77d80829
betolguin <lolguinv@uc.cl>
2025-03-22 20:40:25 -0300

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,17 +6,17 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: '#f5f3ff',
-          100: '#ede9fe',
-          200: '#ddd6fe',
-          300: '#c4b5fd',
-          400: '#a78bfa',
-          500: '#8b5cf6',
-          600: '#7c3aed',
-          700: '#6d28d9',
-          800: '#5b21b6',
-          900: '#4c1d95',
-          950: '#2e1065',
+          50: '#eaf5fa',    // Fondo claro
+          100: '#7cace4',   // Tono suave
+          200: '#78a1dc',   // 
+          300: '#5a81b7',   // 
+          400: '#3b6ca6',   // 
+          500: '#2f5c9d',   // Color principal
+          600: '#044bab',   // Hover/énfasis
+          700: '#164789',   // Active/bordes
+          800: '#164789',   // Textos importantes
+          900: '#164789',   // 
+          950: '#164789',   // 
         },
         secondary: {
           50: '#f8fafc',
